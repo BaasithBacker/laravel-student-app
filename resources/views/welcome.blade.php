3bc019d51ed1fba318eb35b80d3bfafbef62a216
BaasithBacker <bh.BasithBacker07@gmail.com>
2021-03-17 12:44:26 +0530

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,7 @@
     <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
     </div>
     <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <h1>Welcome</h1>
     <table class="table">
     <tr>
         <td>Name</td>
