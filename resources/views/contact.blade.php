5dc5f7f7e20d98ead19085fe51c659e0d64d5bea
BaasithBacker <bh.BasithBacker07@gmail.com>
2021-03-17 12:39:17 +0530

    updated

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>contact</title>
 </head>
 <body>
 
@@ -31,11 +31,12 @@
   </div>
 </nav>
 
+
     <div class="container">
-    </div>
     <div class="row">
     <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
     </div>
+
     <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
     <table class="table">
     <tr>
@@ -43,15 +44,11 @@
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Roll-No</td>
-        <td><input type="text" class="form-control"></td>
+        <td>Message</td>   
+        <td><textarea name="" id="" cols="70" rows="10" class="from-control"></textarea></td>
     </tr>
     <tr>
-        <td>Admission-No</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>College</td>
+        <td>Phone-No</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
@@ -59,11 +56,13 @@
         <td><button class="btn btn-outline-success">Submit</button></td>
     </tr>
     </table>
-    </div>
+    </div> 
     <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
     </div>
+
     </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 </body>
 </html>
